cb6d82f3fb507b4619ad9ef2c045900ad25e43b1
Matthew de Detrich <mdedetrich@gmail.com>
2015-07-28 11:03:45 +1000

    - Added an extra test for JBoolean

--- jvm/src/test/scala/BigDecimalRuntime.scala
+++ jvm/src/test/scala/BigDecimalRuntime.scala
@@ -4,7 +4,6 @@ import org.scalameter.api._
 object BigDecimalRuntime extends Microbenchmark {
 
   val sizes: Gen[Int] = Gen.range("size")(300000, 1500000, 300000)
-
   
   // Lets assume our input is an Array of characters, likely to be similar to what you would receive in JSON
   val chars: Gen[Array[Char]] = for {
