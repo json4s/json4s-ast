8fb3116ea44ffaa717996ae2061e29d56a03e087
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-10-09 07:44:28 +0200

    Fix #10 on the JVM

--- jvm/src/main/scala/org/json4s/ast/safe/JValue.scala
+++ jvm/src/main/scala/org/json4s/ast/safe/JValue.scala
@@ -53,7 +53,7 @@ sealed abstract class JBoolean extends JValue {
 
 object JBoolean {
   def apply(x: Boolean): JBoolean = if (x) JTrue else JFalse
-  def unapply(x: JBoolean): Some[Boolean] = Some(x.isEmpty)
+  def unapply(x: JBoolean): Some[Boolean] = Some(x.get)
 }
 
 case object JTrue extends JBoolean {
